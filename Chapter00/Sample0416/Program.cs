763260d8d6eb3425f4c472ddb2648f84b1ab8f79
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-04-23 10:07:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0416/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Sample0416 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int i = 0;
+            while (i < 10) {
+                Console.WriteLine("Hello, World!");
+                i++;
+            }
+            Console.WriteLine(i + "回繰り返しました");
+        }
+    }
+}
---
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public static class FeetConverter {
+        private const double ratio = 0.3048; //定数
+
+        // メートルからフィートを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
