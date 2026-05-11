3a9a1d2ec7c3c3f0a988a4d675cf4375bb4064f2
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-05-11 10:44:18 +0900

    問題2.1完成

--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,38 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SalesCalculator {
-    //売り上げ集計クラス
+    // 売り上げ集計クラス
     public class SalesCounter {
-        private readonly List<Sale> _sales;
 
-        //コンストラクター
-        public SalesCounter(List<Sale>sales) {
-            _sales = sales;
+        private readonly IEnumerable<Sale> _sales;
+
+        // コンストラクター
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
-        //店舗別売り上げを求める   
-        public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
+        public List<Sale> ReadSales(string filePath) {
+
+            List<Sale> sales = new List<Sale>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines) {
+
+                var items = line.Split(',');
+
+                var sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
+
+        // 店舗別売り上げを求める
+        public IDictionary<string, int> GetPerStoreSales() {
+
+            var dict = new SortedDictionary<string, int>();
 
             foreach (var sale in _sales) {
+
                 if (dict.ContainsKey(sale.ShopName)) {
-                    // 既に登録されている場合：加算
                     dict[sale.ShopName] += sale.Amount;
                 } else {
-                    // 未登録の場合：新しく登録
                     dict[sale.ShopName] = sale.Amount;
-                   
                 }
             }
 
             return dict;
         }
-
-
     }
 }
