b6f69114be5eb9ffd1e6f4a1a90d322deb6b6799
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-05-21 11:17:00 +0900

    問題3.1完成

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -4,24 +4,25 @@
        
         
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            //Judgement judge = IsEven;
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            var count = Count( numbers, n=> n % 4 == 0 || n % 5 == 0);
-            Console.WriteLine(count);
+            var exists = cities.Exists(s => s[0] == 'A');
+            Console.WriteLine(exists);
+
         }
 
       
 
-        static int Count(int[] numbers, Predicate<int> judge) {
-            var count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
-        }
+
     }
 }
---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            //3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            //3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+
+            //3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+
+            //3.1.4
+            Exercise4(numbers);
+
+        }
+
+        private static void Exercise1(List<int> numbers) {
+
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exist == true) {
+                Console.WriteLine("存在します");
+            }else {
+                Console.WriteLine("存在しません");
+            }
+        }
+
+        private static void Exercise2(List<int> numbers) {
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
+        }
+
+        private static void Exercise3(List<int> numbers) {
+            numbers.Where(n => n > 50).ToList().ForEach(n => Console.WriteLine(n));
+        }
+
+        private static void Exercise4(List<int> numbers) {
+            var select = numbers.Select(n => n * 2);
+            
+            foreach(var n in select) {
+                Console.WriteLine($"{n}");
+            }
+        }
+    }
+}
