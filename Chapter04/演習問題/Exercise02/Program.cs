d4098445dba5b35eb6be27ce1bea30f1ef5e4b8a
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-05-27 12:05:59 +0900

    問題4.2の1まで

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -10,9 +10,27 @@ namespace Exercise02 {
             Exercise3();
         }
 
+        
+
         private static void Exercise1() {
             //if-else文を使用
-            
+
+
+            Console.Write("点数を入力してください: ");
+
+            int score = int.Parse(Console.ReadLine());
+
+            if (score < 0) {
+                Console.WriteLine(score);
+            }else if (score < 100) {
+                Console.WriteLine(score * 2);
+            } else if (score < 500) {
+                Console.WriteLine(score * 3);
+            } else {
+                Console.WriteLine(score);
+            }
+
+
         }
 
         private static void Exercise2() {
@@ -22,7 +40,6 @@ namespace Exercise02 {
 
         private static void Exercise3() {
             //switch式を使用
-
 
         }
     }
