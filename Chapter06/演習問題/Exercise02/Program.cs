c19719bfdda9974ffedb6a6df0f031f65bcfb2c4
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-06-10 12:05:25 +0900

    問題6.3.4まで

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var int1 = Console.ReadLine();
+
+            if(int.TryParse(int1,out int number)) {
+                string result = number.ToString("#,0");
+                Console.WriteLine(result);
+            } else {
+                Console.WriteLine("整数文字列ではありません");
+            }
+
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            int count = text.Count(c => c == ' ');
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2(string text) {
+
+            Console.Write("検索");
+            var search = Console.ReadLine();
+            Console.Write("置換");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            
+        }
+
+        private static void Exercise4(string text) {
+            int count2 = text.Split(' ').Length;
+            Console.WriteLine(count2);
+        }
+
+        private static void Exercise5(string text) {
+            
+        }
+
+        private static void Exercise6(string text) {
+            
+        }
+    }
+}
