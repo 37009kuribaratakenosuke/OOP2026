6e5a4615bd8e73389f0258ff98731e3cf56fb849
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-06-15 10:35:57 +0900

    カンマ区切りで出力

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var separator = ",";
+            var result = String.Join(separator, GetWords());
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<string> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+
+        }
+    }
+}
