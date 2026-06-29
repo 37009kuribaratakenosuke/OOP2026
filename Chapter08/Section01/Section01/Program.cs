4d2654e1be50e53824cae4bef12acc97d0b70bef
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-06-29 14:32:45 +0900

    演習問題　完成

--- Chapter08/Section01/Section01/Program.cs
+++ Chapter08/Section01/Section01/Program.cs
@@ -22,61 +22,101 @@
 
                 if (prefCaptalLocation == null) break;
 
+                if (preOfficeDict.ContainsKey(pref)) {
+                    Console.WriteLine("上書きしますか？(Y/N");
+                    if (Console.ReadLine() == "N") continue;
+                }
+
                 //県庁所在地登録処理 
 
                 preOfficeDict[pref] = prefCaptalLocation;
 
             }
-            Menu();
+
+            Boolean endFlag = false;
+            while (true) {
+                switch (menuDisp()) {
+                    case 1:
+                        allDisp();
+                        break;
+                    case 2:
+                        searchPre();
+                        break;
+
+                default:
+                        endFlag = true;
+                        break;
+                }
+            }
+
         }
 
-                static void Menu() {
-                    while (true) {
-                        Console.WriteLine();
-                        Console.WriteLine("**** メニュー ****");
-                        Console.WriteLine("1: 一覧表示");
-                        Console.WriteLine("2: 検索");
-                        Console.WriteLine("9: 終了");
-                        Console.Write("選択:");
+        //        static void Menu() {
+        //            while (true) {
+        //                Console.WriteLine();
+        //                Console.WriteLine("**** メニュー ****");
+        //                Console.WriteLine("1: 一覧表示");
+        //                Console.WriteLine("2: 検索");
+        //                Console.WriteLine("9: 終了");
+        //                Console.Write("選択:");
 
-                        string? menu = Console.ReadLine();
+        //                string? menu = Console.ReadLine();
 
-                        switch (menu) {
-                            case "1":
-                                foreach (var item in preOfficeDict) {
-                                    Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です");
-                                }
-                                break;
+        //                switch (menu) {
+        //                    case "1":
+        //                        foreach (var item in preOfficeDict) {
+        //                            Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です");
+        //                        }
+        //                        break;
 
-                            case "2":
-                                Console.Write("都道府県 :");
-                                string? pref = Console.ReadLine();
+        //                    case "2":
+        //                        Console.Write("都道府県 :");
+        //                        string? pref = Console.ReadLine();
 
-                                if (pref != null && preOfficeDict.ContainsKey(pref)) {
-                                    Console.WriteLine($"県庁所在地: {preOfficeDict[pref]}");
-                                } 
-                                break;
+        //                        if (pref != null && preOfficeDict.ContainsKey(pref)) {
+        //                            Console.WriteLine($"県庁所在地: {preOfficeDict[pref]}");
+        //                        } 
+        //                        break;
 
-                            case "9":
-                                Console.WriteLine("終了");
-                                return;
+        //                    case "9":
+        //                        Console.WriteLine("終了");
+        //                        return;
 
-                        }
+        //                }
 
-                    }
-                }
-
-        //private static int menuDisp() {
-        //    Console.WriteLine();
-        //    Console.WriteLine("**** メニュー ****");
-        //    Console.WriteLine("1: 一覧表示");
-        //    Console.WriteLine("2: 検索");
-        //    Console.WriteLine("9: 終了");
-        //    Console.Write(">");
-
-        //    return  
+        //            }
         //}
 
-    }
+        private static int menuDisp() {
+            Console.WriteLine();
+            Console.WriteLine("**** メニュー ****");
+            Console.WriteLine("1: 一覧表示");
+            Console.WriteLine("2: 検索");
+            Console.WriteLine("9: 終了");
+            Console.Write(">");
+
+            var menuSelect = int.Parse(Console.ReadLine());
+
+            return menuSelect;
+            }
+
+        private static void allDisp() {
+            foreach(var p in preOfficeDict) {
+                Console.WriteLine($"{p.Key}の県庁所在地は{p.Value}です");
+            }
         }
+
+        private static void searchPre() {
+            Console.Write("都道府県:");
+            var searchPref = Console.ReadLine();
+
+            if (searchPref is null) return;
+
+            if (preOfficeDict.ContainsKey(searchPref)) {
+                Console.WriteLine($"{searchPref}の県庁所在地は{preOfficeDict[searchPref]}です");
+            } 
+        }
+
+    }
+    }
   
