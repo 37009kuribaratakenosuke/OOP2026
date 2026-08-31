6fde8ccdc6bdd0ff4026ce93e4c52fd23ebf961a
武之介 栗原 <oibc37009@ojs.ac.jp>
2026-08-31 15:47:59 +0900

    From1への一覧表示

--- FormApps/SQLiteProductSample/Form1.cs
+++ FormApps/SQLiteProductSample/Form1.cs
@@ -12,6 +12,14 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+
+        dgvProducts.AutoGenerateColumns = true;
+
+        dgvProducts.DataSource = _products;
+
+        ReloadProducts();
+
+        tsslMessage.Text = $"DB: {Database.FilePath}";
     }
 
     private void btAdd_Click(object sender, EventArgs e)
@@ -41,7 +49,11 @@ public partial class Form1 : Form
 
     private void ReloadProducts()
     {
-       
+        _products.Clear();
+        foreach (var product in _repository.GetAll()) {
+            _products.Add(product);
+        }
+        dgvProducts.ClearSelection();
     }
 
     private bool TryGetInput(out string name, out int price)
